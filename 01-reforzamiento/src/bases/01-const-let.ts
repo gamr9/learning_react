be1df718c85cb30677a4924e3d992eb8e52f84f1
Gustavo Mora <gustavo@netcommerce.mx>
2025-09-23 23:27:10 -0600

    feture/first practice with Typescript

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,17 @@
+const firstName: string = 'Gustavo' ; 
+const lastName = 'Mora' ; 
+
+console.log(firstName, lastName);
+
+const includeLetterM = lastName.includes('M');
+
+let diceNumber = 20;
+
+diceNumber = 0;
+
+/**
+ * Error
+ * diceNumber = '0';
+ */
+
+console.log( {includeLetterM, diceNumber} )
