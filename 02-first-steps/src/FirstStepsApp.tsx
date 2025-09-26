13b4d73413f6422c41cc99fcb60bdc9781c69446
Gustavo <gustavo.mora@resser.com>
2025-09-26 13:24:09 -0600

    feature/finish first section

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,14 +1,29 @@
+import { ItemCounter } from "./shopping-cart/ItemCounter";
+
+interface Item {
+    productName: string;
+    quantity: number;
+}
+
+const Items: Item[] = [
+    {productName:'Switch', quantity: 4 },
+    {productName:'Play', quantity: 5 },
+    {productName:'Xbox', quantity: 4}
+];
+
 export function FirstStepsApp(){
     return(
         <>
-            <h1>Hola mundo :D</h1>
-            <p>Esto es una prueba</p>
-
-            <button>Click me</button>
-
-            <div>
-                <h2>Hola dentro de un div</h2>
-            </div>
+            <h1>Carrito de compras</h1>
+            {
+                Items.map(({ productName, quantity }) => (
+                    <ItemCounter key={productName} name={productName} quantity={quantity} />
+                ))
+            }
+            {/* <ItemCounter name='Switch' quantity={1} />
+            <ItemCounter name='Play' quantity={2} />
+            <ItemCounter name='Xbox' quantity={3} /> */}
+           
         </>
     );
 }
--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useState } from "react";
 
-export const ItemCounter = () => {
-  return (
-    <div>ItemCounter</div>
-  )
+
+import './ItemCounter.css';
+
+interface Props {
+  name: string;
+  quantity?: number;
 }
+
+export const ItemCounter = ({name, quantity = 1}: Props) => {
+  
+  const [count, setCount]  = useState(quantity);
+
+  const handdleAdd = (count: number) => {
+    setCount(count + 1);
+  };
+
+  const handdleSubtrack = (count: number) => {
+    if (count === 1) return;
+
+    setCount(count - 1);
+  };
+
+  const handleClick = () => {
+    console.log(`Click en ${count + 1}`)
+    console.log(`Click en ${count}`)
+  };
+
+  return (
+    <section className="item-row">
+      <span className="item-text" style={{ color: count === 1 ? 'red' : 'black' }}>{name}</span>
+      <button onClick={() => { handdleAdd(count); handleClick(); }}>
+        +1
+      </button>
+      <span>{count}</span>
+      <button onClick={() => { handdleSubtrack(count); handleClick(); }}>
+        -1
+      </button>
+    </section>  
+  );
+}
+
